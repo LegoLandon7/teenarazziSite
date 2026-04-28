24a6a79a500c323220be1fe52882b14ca42723b8
Landon Lego <legolandon25@gmail.com>
2026-04-28 13:27:18 +0000

    fixed typo

--- frontend/src/pages/Socials.tsx
+++ frontend/src/pages/Socials.tsx
@@ -17,7 +17,7 @@ export default function Socials() {
                     { social: 'reddit', link: 'https://reddit.com/r/teenarazzi', username: 'r/teenarazzi' },
                     { social: 'discord', link: 'https://discord.gg/py9ePyMdec', username: 'Discord Server' },
                     { social: 'instagram', link: 'https://instagram.com/teenarazzi', username: 'Instagram' },
-                    { social: 'fandom', link: 'https://teenarazzi.fandom.com/wiki/Teenarazzi_Wiki', username: 'Teenrazzi Wiki' },
+                    { social: 'fandom', link: 'https://teenarazzi.fandom.com/wiki/Teenarazzi_Wiki', username: 'Teenarazzi Wiki' },
                     { social: 'email', link: 'mailto:contact@teenarazzi.com', username: 'contact@teenarazzi.com' },
                     
                 ]}
